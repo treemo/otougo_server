7982d6af75b4bc0b37a6f8ea60a42fa43900b68e
Treemo <matthieu.chevrier@treemo.fr>
2014-04-11 23:50:03 +0200

    ajout des stats sur les requettes serveurs

---
+++ lib/stats/Stats.class.php
@@ -0,0 +1,31 @@
+<?php
+
+include_once __DIR__ . '/../../config/load.php';
+include_once __DIR__ . '/../mysql/Sql.class.php';
+
+class Stats {
+
+	public static function addAction($action, $dataList) {
+
+        $sql = new Sql();
+
+        $sql->prepare('INSERT INTO stat_use_' . $action . ' (' . implode(',', array_keys($dataList)) . ') VALUES (:' . implode(',:', array_keys($dataList)) . ')');
+        
+        foreach($dataList as $name => $data) {
+            $sql->bindValue(":$name", $data);
+        }
+
+        return $sql->execute();
+	}
+    
+    public static function addActionGetMarker($latitude, $longitude, $distance, $timeUpdate = 0, $filtre = '') {
+
+        self::addAction('get_marker', array(
+            'latitude' => $latitude,
+            'longitude' => $longitude,
+            'distance' => $distance,
+            'timeUpdate' => $timeUpdate,
+            'filtre' => $filtre,
+        ));
+	}
+}
