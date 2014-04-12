b505d64089b48af9ee33ffdc97193f50989a3138
Florian <florian.gomis@viacesi.fr>
2014-04-12 15:27:49 +0200

    decommente les ajout en bdd

--- import/update.php
+++ import/update.php
@@ -9,7 +9,7 @@ include __DIR__ . '/../lib/marker/MarkerManager.class.php';
 include __DIR__ . '/../lib/functions.php';
 
 $dirList = array(
-   /* 'bicycle' => array(
+    'bicycle' => array(
         'jcdecaux',
     ),
     'parking' => array(
@@ -20,7 +20,7 @@ $dirList = array(
         'pointArretEURE',
         'tcar', 
         'horraireTrain',
-    ),*/
+    ),
     'pooling' => array(
         'pooling',
     ),
