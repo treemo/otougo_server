10da1ca5ac890c658814c3e127ba9207f7233984
Treemo <matthieu.chevrier@treemo.fr>
2014-04-12 11:48:44 +0200

    maj include update

--- import/update_10min.php
+++ import/update_10min.php
@@ -1,4 +1,4 @@
 <?php
-include __DIR__ . 'update_define.php';
+include __DIR__ . '/update_define.php';
 define('UPDATE_TYPE', TEN_MIN);
 include __DIR__ . 'update.php';
--- import/update_hour.php
+++ import/update_hour.php
@@ -1,4 +1,4 @@
 <?php
-include __DIR__ . 'update_define.php';
+include __DIR__ . '/update_define.php';
 define('UPDATE_TYPE', HOUR);
 include __DIR__ . 'update.php';
